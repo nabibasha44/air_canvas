a979d60bd1e31778853f7b4a4853ac121b7b57d4
basha <ma.nabibasha44@gmail.com>
2023-02-02 12:39:25 +0530

    Updated requirement.txt

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, url_for, Response
+from flask import Flask, Response
 from hand_finger_motion import access_camera
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return {"path": 'http://127.0.0.1:5000/video'}
 
 
 @app.route('/video')
