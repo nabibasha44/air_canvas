9170ed39e33f28f5e3e97d3897eb12bdb2bff9a6
basha <ma.nabibasha44@gmail.com>
2023-02-02 12:17:04 +0530

    Update the colours

--- hand_finger_motion.py
+++ hand_finger_motion.py
@@ -5,7 +5,7 @@ import mediapipe as mp
 from collections import deque
 
 
-def access_camera():
+def access_camera(clear=None):
 
     # Giving different arrays to handle colour points of different colour
     bpoints = [deque(maxlen=1024)]
@@ -22,23 +22,11 @@ def access_camera():
     # The kernel to be used for dilation purpose
     kernel = np.ones((5, 5), np.uint8)
 
-    colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
+    colors = [(1, 1, 1), (0, 255, 0), (255, 0, 0), (0, 255, 255)]
     colorIndex = 0
 
     # Here is code for Canvas setup
     paintWindow = np.zeros((471, 636, 3)) + 255
-    # paintWindow = cv2.rectangle(paintWindow, (40, 1), (140, 65), (0, 0, 0), 2)
-    # paintWindow = cv2.rectangle(paintWindow, (160, 1), (255, 65), (255, 0, 0), 2)
-    # paintWindow = cv2.rectangle(paintWindow, (275, 1), (370, 65), (0, 255, 0), 2)
-    # paintWindow = cv2.rectangle(paintWindow, (390, 1), (485, 65), (0, 0, 255), 2)
-    # paintWindow = cv2.rectangle(paintWindow, (505, 1), (600, 65), (0, 255, 255), 2)
-
-    # cv2.putText(paintWindow, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    # cv2.putText(paintWindow, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    # cv2.putText(paintWindow, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    # cv2.putText(paintWindow, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    # cv2.putText(paintWindow, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    # cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
 
     # initialize mediapipe
     mpHands = mp.solutions.hands
@@ -60,14 +48,14 @@ def access_camera():
         framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         frame = cv2.rectangle(frame, (40, 1), (140, 65), (0, 0, 0), 2)
-        frame = cv2.rectangle(frame, (160, 1), (255, 65), (255, 0, 0), 2)
+        frame = cv2.rectangle(frame, (160, 1), (255, 65), (1, 1, 1), 2)
         frame = cv2.rectangle(frame, (275, 1), (370, 65), (0, 255, 0), 2)
-        frame = cv2.rectangle(frame, (390, 1), (485, 65), (0, 0, 255), 2)
+        frame = cv2.rectangle(frame, (390, 1), (485, 65), (255, 0, 0), 2)
         frame = cv2.rectangle(frame, (505, 1), (600, 65), (0, 255, 255), 2)
-        cv2.putText(frame, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-        cv2.putText(frame, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (250, 250, 250), 2, cv2.LINE_AA)
+        cv2.putText(frame, "BLACK", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
         cv2.putText(frame, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-        cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, "BLUE", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
         cv2.putText(frame, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
         #frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
@@ -106,6 +94,7 @@ def access_camera():
                 yellow_index += 1
 
             elif center[1] <= 65:
+                print("Clearing", center[1])
                 if 40 <= center[0] <= 140: # Clear Button
                     bpoints = [deque(maxlen=912)]
                     gpoints = [deque(maxlen=512)]
