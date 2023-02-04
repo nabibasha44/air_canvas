925d456eb755e756ec94d7ddff26fbacdd793c36
basha <ma.nabibasha44@gmail.com>
2023-02-04 15:39:10 +0530

    Integrated handwritten recognition with air canvas

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, Response
 from hand_finger_motion import access_camera
 from flask_cors import CORS
+import base64, os
+from hand_written_rec import detect_hand_written
 
 app = Flask(__name__)
 cors = CORS(app)
+
+
+responses = {}
 
 
 @app.route("/")
@@ -14,6 +19,30 @@ def main():
 @app.route('/video')
 def video():
     return Response(access_camera(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/text-response', methods=['POST'])
+def ocr_response(request):
+    file_body = request.body()
+    file_content = file_body.get('buff_data')
+    try:
+        file_content = base64.b64decode(file_content)
+        os.makedirs(os.getcwd() + '/tmp', exist_ok=True)
+        localdir = os.getcwd()+'/tmp/'
+        temp_files = os.listdir(localdir)
+        temp_files = [int(j.lstrip('_').strip('.png')) for j in temp_files]
+        temp_version = temp_files[-1]
+        localfile = f'{localdir}_{temp_version}.png'
+        with open(localfile, "wb") as f:
+            f.write(file_content)
+
+        result = detect_hand_written(localfile)
+        responses.update({str(temp_version): result})
+        return {'status': 'success'}
+
+    except Exception as e:
+        print(str(e))
+        return {'status': 'fail'}
 
 
 if __name__ == "__main__":
---
+++ hand_written_rec.py
@@ -0,0 +1,13 @@
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
+
+
+def detect_hand_written(img):
+    image = Image.open(img).convert("RGB")
+
+    processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
+    model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten')
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    generated_ids = model.generate(pixel_values)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return generated_text
